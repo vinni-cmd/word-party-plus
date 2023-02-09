464448cb24e7fc7caeb13187c4593416d847cc37
vincentYoung <vinni.xander@gmail.com>
2023-02-09 14:55:38 -0700

    add firebase db module

---
+++ src/modules/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCR1-jTMmp5MCPmYgTxkC9M_rneJf0R82Q",
+  authDomain: "word-party-7947f.firebaseapp.com",
+  databaseURL: "https://word-party-7947f-default-rtdb.firebaseio.com",
+  projectId: "word-party-7947f",
+  storageBucket: "word-party-7947f.appspot.com",
+  messagingSenderId: "703213887524",
+  appId: "1:703213887524:web:47005cb362841c90c4f8b1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
