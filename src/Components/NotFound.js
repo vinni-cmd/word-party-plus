9b2519c032e8789e8710deac0895d0c8e9e0f54b
vincentYoung <vinni.xander@gmail.com>
2023-02-17 13:26:23 -0700

    update styles and implement semantic html

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -7,10 +7,14 @@ const NotFound = () => {
       <img className="not-found-logo" src={errorLogo} alt="logo for page not found" />
       <h2>Oops! You seem to be lost.</h2>
       <p>Here are some helpful links:</p>
-      <div className="not-found-links">
-        <Link to="/">Home</Link>
-        <Link to="/SavedWords">Saved Words</Link>
-      </div>
+      <ul className="not-found-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/SavedWords">Saved Words</Link>
+        </li>
+      </ul>
     </div>
   );
 }
