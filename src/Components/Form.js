2fd98f3a15f6e10dfd9e210f58fa60381a7ba869
vincentYoung <vinni.xander@gmail.com>
2023-03-16 12:27:56 -0600

    refine form component

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,5 +1,4 @@
 // modules
-import { useState } from "react";
 import { GiPartyPopper } from "react-icons/gi"
 // local imports
 import throwAlert from "../modules/alerts";
@@ -10,9 +9,10 @@ const Form = ({
   setWordResultList,
   setSearchWord,
   searchWord,
+  currentCategory,
+  setCurrentCategory,
   setCurrentCategoryName,
 }) => {
-  const [currentCategory, setCurrentCategory] = useState("");
   // handler for form submission
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -64,32 +64,23 @@ const Form = ({
           <option disabled value="">
             Please Select
           </option>
+          <option value="trg" title="Words that are strongly associated with your input" aria-label="Words that are strongly associated with your input" required>
+            Triggers *
+          </option>
           <option value="jjb" required>
             Adjectives
-          </option>
-          <option value="ant" required>
-            Antonyms
-          </option>
-          <option value="cns" required>
-            Consonant Match
-          </option>
-          <option value="hom" required>
-            Homophones
-          </option>
-          <option value="nry" required>
-            Near Rhymes
-          </option>
-          <option value="jja" required>
-            Nouns
-          </option>
-          <option value="rhy" required>
-            Rhymes
           </option>
           <option value="syn" required>
             Synonyms
           </option>
-          <option value="trg" required>
-            Triggers
+          <option value="ant" required>
+            Antonyms
+          </option>
+          <option value="rhy" required>
+            Rhymes
+          </option>
+          <option value="jja" title="Popular nouns modified by the given adjective" aria-label="Popular nouns modified by the given adjective" required>
+            Nouns *
           </option>
         </select>
         <button onSubmit={handleSubmit} title="Submit search" aria-label="Submit search"><GiPartyPopper aria-hidden="true" /></button>
